66ab1ecd3425824b5c7269bb69305fbcc21bd0ba
Noura <culurienneldoreth@gmail.com>
2013-04-06 20:05:28 -0400

    fixed tabs bug

--- static/js/app.js
+++ static/js/app.js
@@ -5,9 +5,8 @@ $(window).load(function() {
     var cutoff_width = tab_content_width - tab_width;
     var tabs = [];
     $('.tab').each(function() {
-        tabs.push({$el: $(this), fixed: false, active: false});
+        tabs.push({$el: $(this)});
     });
-    tabs[0].active = true;
 
     $('.tab').on('click', function() {
         var i = $(this).data('i');
@@ -22,29 +21,20 @@ $(window).load(function() {
         }
         var margin_left = i*tab_width;
         var offset = (i+1)*tab_content_width;
-        if (!tabs[i].fixed) {
-            tabs[i].$el.css({'position': 'fixed', 'margin-left': margin_left+'px'});
-            tabs[i].fixed = true;
-        }
+        tabs[i].$el.css({'position': 'fixed', 'margin-left': margin_left+'px'});
         for (var j = i + 1; j < tabs.length; j++) {
-            if (tabs[j].fixed) {
-                tabs[j].$el.css({'position': 'static', 'margin-left': '0'});
-                tabs[j].fixed = false;
-            }
+            tabs[j].$el.css({'position': 'static', 'margin-left': '0'});
         }
         if (i + 1 < tabs.length) {
             tabs[i+1].$el.css('margin-left', offset+'px');
         }
-        _.each(tabs, function(tab, j) {
-            if (j == i && !tab.active) {
-                tab.$el.addClass('active');
-                tab.active = true;
+        for (var j = 0; j < tabs.length; j++) {
+            if (j == i) {
+                tabs[j].$el.addClass('active');
+            } else {
+                tabs[j].$el.removeClass('active');
             }
-            if (j != i && tab.active) {
-                tab.$el.removeClass('active');
-                tab.active = false;
-            }
-        });
+        }
     }, 50);
 });
 
