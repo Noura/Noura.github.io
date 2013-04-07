ee9d662eb08d0d6f9345b3a008d719d93d0ddb4a
Noura <culurienneldoreth@gmail.com>
2013-04-06 21:29:22 -0400

    made scrolling to the far right of page end nicely

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,14 @@
 $(window).load(function() {
+
+    // make content the right width to finish scrolling nicely in this window
+    var extra_width = $(window).width() - 700 - 151 - 21;
+    if (extra_width > 0) {
+        var $main = $('#main');
+        var main_width = $main.width();
+        $main.width(main_width + extra_width);
+    }
+   
+    // constants used throughout
     var $body = $('body');
     var tab_content_width = 800;
     var tab_width = 102;
@@ -8,11 +18,13 @@ $(window).load(function() {
         tabs.push({$el: $(this)});
     });
 
+    // clicking on a tab scrolls you there
     $('.tab').on('click', function() {
         var i = $(this).data('i');
         $body.animate({scrollLeft: i*tab_content_width}, 500);
     });
 
+    // scrolling through tabs
     setInterval(function() {
         var x = Math.max(0, $body.scrollLeft());
         var i = Math.floor(x / cutoff_width);
