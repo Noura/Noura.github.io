29a2222cfc71bd0273cb078fe5cfcddb84c2a42e
Noura <culurienneldoreth@gmail.com>
2014-06-27 19:17:03 -0700

    kind of like this new layout but still working on it

--- make_pages.py
+++ make_pages.py
@@ -3,29 +3,66 @@ import os, os.path, shutil, codecs, sys
 
 import jinja2
 
-pages = [ {
-            'template': 'index.html',
-            'path': '',
-            'tabname': 'Projects',
-            'projects': ['mobile_illiteracy.html', 'solar_cooker.html', 'waazam.html', 'sxm.html', 'qa.html'],
-          },
-          {
-            'template': 'index.html',
-            'path': 'experiments',
-            'tabname': 'Experiments',
-            'projects': ['lissajous.html', 'chladni.html'],
-          },
-          {
-            'template': 'about.html',
-            'path': 'about',
-            'tabname': 'About'
-          }
-        ]
+pages = [ \
+    {
+        'name': 'Mobile Phones & Illiteracy in Morocco',
+        'thumbnail': 'apples',
+        'path': 'moblit',
+        'template': 'mobile_illiteracy.html',
+    },
+    {
+        'name': 'Solar Cooker for the Himalayas',
+        'thumbnail': 'solsource',
+        'path': 'solsource',
+        'template': 'solar_cooker.html',
+    },
+    {
+        'name': 'WaaZam!',
+        'thumbnail': 'waazam',
+        'path': 'waazam',
+        'template': 'waazam.html',
+    },
+    {
+        'name': 'SiriusXM Radio Station Creator',
+        'thumbnail': '',
+        'path': 'sxm',
+        'template': 'sxm.html',
+    },
+    {
+        'name': 'Tools for Quality Assurance',
+        'thumbnail': '',
+        'path': 'en_qa',
+        'template': 'qa.html',
+    },
+    {
+        'name': 'Lissajous & Rotary Harmonograph Visualizer',
+        'thumbnail': 'lissa',
+        'path': 'lissa',
+        'template': 'lissajous.html',
+    },
+    {
+        'name': 'Chladni Waves Visualizer',
+        'thumbnail': 'chladni',
+        'path': 'chladni',
+        'template': 'chladni.html',
+    },
+    {
+        'name': 'About Noura Howell',
+        'thumbnail': '',
+        'path': 'about',
+        'template': 'about.html',
+    },
+  ]
 
 def main():
     here = os.path.dirname(__file__)
     loader = jinja2.FileSystemLoader(os.path.join(here, 'templates'))
     templates = jinja2.Environment(loader=loader)
+
+    tem = templates.get_template('index.html')
+    ctx = { 'pages': pages }
+    with codecs.open(os.path.join(here, 'index.html'), 'w') as out:
+        out.write(tem.render(**ctx))
 
     for page in pages:
         tem = templates.get_template(page['template'])
