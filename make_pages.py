bcaf08aab630d6cef644c5cc490d7d62bd3df7b2
Noura <culurienneldoreth@gmail.com>
2015-06-25 01:00:47 -0700

    adding a stub about project jacquard

--- make_pages.py
+++ make_pages.py
@@ -5,6 +5,13 @@ import jinja2
 
 pages = [ \
     {
+        'name': 'Design / Jacquard',
+        'thumbnail': 'jacquard',
+        'path': 'jacquard',
+        'template': 'jacquard.html',
+        'date': '2015',
+    },
+    {
         'name': 'Buddy the HugBug',
         'thumbnail': 'hugbug',
         'path': 'hugbug',
