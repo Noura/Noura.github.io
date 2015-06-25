c30180ea9dd372312274188d637958ee550a68c2
Noura <culurienneldoreth@gmail.com>
2015-06-25 00:07:29 -0700

    if you go to a page with a hash it will take you to that project page

--- static/js/app.js
+++ static/js/app.js
@@ -4,9 +4,15 @@ $(window).load(function() {
         var project_id = $this.data('project');
         $('.container').addClass('hide');
         $('.container.project-page[data-project="'+project_id+'"]').removeClass('hide');
+        window.location.hash = project_id;
     });
     $('.close-project-page').click(function(e) {
         $('.container').addClass('hide');
         $('.container.tiles').removeClass('hide');
+        window.location.hash = '';
     });
+    if (window.location.hash.length > 1) {
+        var project_id = window.location.hash.slice(1);
+        ($('.tile[data-project="'+project_id+'"]')[0]).click();
+    }
 });
