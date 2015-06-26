ac45c726f0b9341edaf989702fafe45ec64d0281
Noura <culurienneldoreth@gmail.com>
2015-06-25 17:02:57 -0700

    simplified hash routing

--- static/js/app.js
+++ static/js/app.js
@@ -1,25 +1,18 @@
-$(window).load(function() {
-    $('.tile').click(function(e) {
-        var $this = $(this);
-        var project_id = $this.data('project');
-        $('.container').addClass('hide');
-        $('.container.project-page[data-project="'+project_id+'"]').removeClass('hide');
-        window.location.hash = project_id;
-    });
-    $('.close-project-page').click(function(e) {
-        $('.container').addClass('hide');
-        $('.container.tiles').removeClass('hide');
-        window.location.hash = '';
-    });
-    function sync_page() {
+$(document).ready(function() {
+    function sync_pages() {
         if (window.location.hash.length > 1) {
+            // show a project page
             var project_id = window.location.hash.slice(1);
-            ($('.tile[data-project="'+project_id+'"]')[0]).click();
+            var $tile = ($('.tile[data-project="'+project_id+'"]')[0]);
+            $('.container').addClass('hide');
+            $('.container.project-page[data-project="'+project_id+'"]').removeClass('hide');
         } else {
+            // hide all project pages, show the home page
             $('.container').addClass('hide');
             $('.container.tiles').removeClass('hide');
         }
+        $(window).scrollTop(0);
     }
-    window.onhashchange = sync_page;
-    sync_page();
+    window.onhashchange = sync_pages;
+    sync_pages();
 });
