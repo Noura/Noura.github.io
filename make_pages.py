99789f1891b9cc8ff7d3a435255180b377c7fcd6
Noura <culurienneldoreth@gmail.com>
2016-12-27 12:24:46 -0500

    handling my publications with a list in python instead of just copy/pasted html everywhere

--- make_pages.py
+++ make_pages.py
@@ -3,12 +3,18 @@ import os, os.path, shutil, codecs, sys
 
 import jinja2
 
+# pages appear in the order listed here
 pages = [ \
     {
+        # appears as title for that project page, and title of tile on homepage
         'name': 'Biosignals as Social Cues',
+        # refers to the image as specified in app.scss
         'thumbnail': 'hint',
+        # the hash of the URL at which the project page appears
         'path': 'hint',
+        # template file in templates/
         'template': 'hint.html',
+        # appears at bottom of project page just as text
         'date': '2015 - present',
     },
     {
@@ -105,21 +111,66 @@ pages = [ \
     {
         'name': 'Contact & Publications',
         'thumbnail': 'nyd2015',
-        'path': 'publications',
-        'template': 'publications.html',
+        'path': 'about',
+        'template': 'about.html',
         'date': '',
     },
   ]
+
+# publications appear in the order listed here
+publications_list = [
+    {
+        # custom name to refer to this publication in templates
+        'name': 'hint',
+        # the citation text, can include HTML, just gets inserted as is
+        'cite': """<b>Noura Howell</b>, Laura Devendorf, Rundong (Kevin) Tian, Tom&aacute;s Vega, Nan-Wei Gong, Ivan Poupyrev, Eric Paulos, Kimiko Ryokai. 2016. Biosignals as social cues: Ambiguity and emotional interpretation in social displays of skin conductance. <i>Designing Interactive Systems</i>.""",
+        # link to pdf or official hosting
+        'url': '/static/pdf/16_DIS_Hint.pdf',
+    },
+    {
+        'name': 'disDC',
+        'cite': """<b>Noura Howell</b>. 2016. Representation and interpretation of biosensing. <i>DIS'16 Companion</i>.""",
+        'url': "/static/pdf/16_DIS_DC.pdf",
+    },
+    {
+        'name': 'ebb',
+        'cite': """Laura Devendorf, Joanne Lo, <b>Noura Howell</b>, Jung Lin Lee, Nan-Wei Gong, M. Emre Karagozler, Shiho Fukuhara, Ivan Poupyrev, Eric Paulos, Kimiko Ryoaki. (2016). "I don't want to wear a screen": Probing perceptions of and possibilities for dynamic displays on clothing. <i>SIGCHI Conference on Human Factors in Computing Systems (CHI'16 - Best Paper Award)</i>.""",
+        'url': "http://artfordorks.com/pubs/16_CHI_Ebb.pdf",
+    },
+    {
+        'name': 'L21',
+        'cite': """Sarah Spence Adams, <b>Noura Howell</b>, Nathaniel Karst, Denise Sakai Troxell, Junjie Zhu. (2013). On the L(2,1)-labelings of amalgamations of graphs. <i>Discrete Applied Mathematics, 161</i>(7-8): 881-8.""",
+        'url': "https://dl.acm.org/citation.cfm?id=2452099",
+    },
+    {
+        'name': 'ethanol',
+        'cite': """Jian Shi, Ratna R. Sharma-Shivappa, Mari Chinn, <b>Noura Howell</b>. (2009). Effect of microbial pretreatment on enzymatic hydrolysis and fermentation of cotton stalks for ethanol production. <i>Biomass and Bioenergy, 33</i>(1): 88-96.""",
+        'url': "http://agris.fao.org/agris-search/search.do?recordID=US201301683178",
+    },
+  ]
+
+# some data munging so that templates can refer to publications by name
+publications_by_name = {}
+for pub in publications_list:
+    publications_by_name[pub['name']] = pub
 
 def make_pages():
     here = os.path.dirname(__file__)
     loader = jinja2.FileSystemLoader(os.path.join(here, 'templates'))
     templates = jinja2.Environment(loader=loader)
 
+    # this is a bit hacky, but, jinja templates like to receive an unpacked dict where the keys of the dict become the variable names in the template. but it was getting too annoying to pass all the variables to different templates individually. now the jinja template just gets one variable ctx which is a dict with all the other variables
+    vars_for_templates = { 
+        'ctx': {
+            'pages': pages,
+            'publications_list': publications_list,
+            'publications_by_name': publications_by_name,
+        }
+    }
+
     tem = templates.get_template('index.html')
-    ctx = { 'pages': pages }
     with codecs.open(os.path.join(here, 'index.html'), 'w') as out:
-        out.write(tem.render(**ctx))
+        out.write(tem.render(**vars_for_templates))
 
     print "make_pages()"
 
