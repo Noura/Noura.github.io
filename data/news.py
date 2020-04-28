06bc1a1e93f6f236f7cd7810e141cd1d948c7b8d
Noura <culurienneldoreth@gmail.com>
2020-04-27 21:11:54 -0700

    moving pages, publications, and news to be in separate data files

---
+++ data/news.py
@@ -0,0 +1,107 @@
+news = [ \
+#     {
+#        'date': "Mar'20",
+#        'description': "Talk at NYU Music and Audio Research Laboratory (MARL) and Integrated Digital Media (IDM) (cancelled)",
+#        'link': None,
+#     },
+#     {
+#        'date': "Mar'20",
+#        'description': "Talk at IST Lisbon in the Department of Computer Science and Engineering",
+#        'link': None,
+#     },
+     {
+        'date': "Dec'19",
+        'description': "Paper accepted to CHI 2020 - Expanding Modes of Reflection in Design Futuring with Sandjar Kozubaev, Chris Elsden, Marie Louise Juul S&oslash;ndergaard, Nick Merrill, Britta Schulte, Richmond Y. Wong",
+        'link': 'https://dl.acm.org/doi/abs/10.1145/3313831.3376526',
+     },
+     {
+        'date': "Feb'20",
+        'description': "Late-Breaking Work accepted to CHI 2020 - Teachable Machine: Approachable Web-Based Tool for Exploring Machine Learning Classification, with Google AI and Google Creative Lab folks Michelle Carney, Barron Webster, Irene Alvarado, Kyle Phillips, Jordan Griffith, Jonas Jongejan, Amit Pitaru, and Alexander Chen",
+        'link': 'https://dl.acm.org/doi/abs/10.1145/3334480.3382839',
+     },
+     {
+        'date': "Mar'20",
+        'description': "Research visit to RITMO with Greg Niemeyer and Alexander Jensenius (cancelled)",
+        'link': 'https://www.uio.no/ritmo/english/',
+     },
+#     {
+#        'date': "Feb'20",
+#        'description': "Talk at Purdue Computer Graphics Technology Department",
+#        'link': None,
+#     },
+#     {
+#        'date': "Feb'20",
+#        'description': "Talk at Georgia Tech School of Literature, Media, and Communication",
+#        'link': None,
+#     },
+#     {
+#        'date': "Jan'20",
+#        'description': "Talk at North Carolina State University Department of Communication",
+#        'link': None,
+#     },
+#     {
+#        'date': "Jan'20",
+#        'description': "Talk at Queen's School of Computing in Kingston, Ontario",
+#        'link': None,
+#     },
+     {
+        'date': "Jan'20",
+        'description': "Grant from Center for Technology Society and Policy joint with Algorithmic Fairness and Opacity Group on the project An Alternate Lexicon for AI with Noopur Raval and Morgan Ames",
+        'link': 'https://ctsp.berkeley.edu/projects-2020/',
+     },
+#     {
+#        'date': "Dec'19",
+#        'description': "Paper accepted to CHI 2020 - Expanding Modes of Reflection in Design Futuring with Sandjar Kozubaev, Chris Elsden, Marie Louise Juul S&oslash;ndergaard, Nick Merrill, Britta Schulte, Richmond Y. Wong",
+#        'link': None,
+#     },
+#     {
+#        'date': "Nov'19",
+#        'description': "Talk at U Michigan School of Information",
+#        'link': None,
+#     },
+#     {
+#        'date': "Oct'19",
+#        'description': "Collaborator Franchesca Spektor presented our work at Center for Technology, Society, and Policy",
+#        'link': 'https://ctsp.berkeley.edu/projects2018/#menstrualBiosensing',
+#     },
+#     {
+#        'date': "Oct'19",
+#        'description': "Talk at Center for Long-Term Cybersecurity Research Exchange",
+#        'link': 'https://cltc.berkeley.edu/grants/speculating-smart-city-cybersecurity-with-the-heart-sounds-bench-detourning-data-and-surveillance-in-public-space/',
+#     },
+#     {
+#        'date': "June'19",
+#        'description': "Paper at DIS on Menstrual Biosensing",
+#        'link': '/projects/menstrual_biosensing.html',
+#     },
+#     {
+#        'date': "May'19",
+#        'description': "Paper at CHI on Life-Affirming Biosensing",
+#        'link': '/projects/heart_sounds_bench.html',
+#     },
+#    {
+#        'date': "Mar'19",
+#        'description': "Talk at InfoCamp Conference",
+#        'link': 'https://berkeley-infocamp.org/',
+#    },
+#    {
+#        'date': "Feb'19",
+#        'description': "Talk at Bay Area Signal Hackers at Pandora",
+#        'link': 'https://www.meetup.com/bishbash/events/258830536/',
+#    },
+#    {
+#        'date': "Feb'19",
+#        'description': "Art Opening at Worth-Ryder Gallery",
+#        'link': 'http://scienceatcal.berkeley.edu/visionlight-processing-perception/',
+#    },
+#    {
+#        'date': "Jan'19",
+#        'description': "Grant from Center for Long-Term Cybersecurity",
+#        'link': 'https://cltc.berkeley.edu/2019grantees/',
+#    },
+#    {
+#        'date': "Jan'19",
+#        'description': "Grant from Center for Technology, Society, & Policy",
+#        'link': 'https://ctsp.berkeley.edu/projects2019/',
+#    }
+]
--- live_updating.sh
+++ live_updating.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 runwatch "templates/*" -r "./make_pages.py" &
+runwatch "data/*" -r "./make_pages.py" &
 
 sass --watch sass/app.scss:static/css/app.css &
 
