4d3b5f916766857ea40b12b536a3d35e6437077f
Noura <culurienneldoreth@gmail.com>
2022-09-23 14:56:13 -0400

    interlaced images

--- interlace_images.py
+++ interlace_images.py
@@ -1,21 +1,22 @@
-#!/Users/noura/.pyenv/shims/python
-
 import os, os.path, subprocess
 
 images_path = 'static/img/'
 
 def main():
     here = os.path.dirname(__file__)
+    print(here)
     top = os.path.join(here, images_path)
+    print(top)
     if not os.path.exists(top):
-        print 'ERROR: path', top, 'not found. make sure you are running this script from the right place'
+        print('ERROR: path', top, 'not found. make sure you are running this script from the right place')
         return
     for root, dirs, files in os.walk(top):
         for f in files:
-            if f[-3:] == 'png' or f[-3:] == 'jpg':
+            fext = f[-3:].lower()
+            if fext == 'png' or fext == 'jpg':
                 img = os.path.join(root, f)
                 cmd = 'convert -interlace line ' + img + ' ' + img
-                print cmd, '\n'
+                print(cmd)
                 subprocess.call(cmd, shell=True)
 
 if __name__ == '__main__':
